a248d25f721c1e61a2b2c7a3ce290d9c26115067
Vilas Patil <patildipak422@gmail.com>
2020-08-31 00:31:34 +0530

    Admission Criteria

---
+++ eligibility_for_admission.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	int maths, physics, chemistry, totalmarks, mathphysics;
+	printf("Enter the Math Marks :");
+	scanf("%d", &maths);
+	printf("Enter the Physics Marks :");
+	scanf("%d", &physics);
+	printf("Enter the Chemistry Marks :");
+	scanf("%d", &chemistry);
+	mathphysics= maths+physics;
+	totalmarks=maths+physics+chemistry;
+	if(maths>=65){
+		if(physics>=55){
+			if(chemistry>=50){
+				if(totalmarks>=180||mathphysics>=140){
+					printf("Congratulation !! You are Eligible for Admission");
+				}
+			}
+		}
+	}
+	else{
+		printf("Sorry You are not eligible for Admissiom");
+	}
+	return 0;
+}
