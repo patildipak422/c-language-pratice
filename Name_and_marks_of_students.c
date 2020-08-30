52bfc99b33ea828037dfc3479aacde65a6505758
Vilas Patil <patildipak422@gmail.com>
2020-08-31 00:33:02 +0530

    calculating the Marks  And percentage of a student

---
+++ Name_and_marks_of_students.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void print(int p, int c, int ca, char name[], int rollno, int totalmarks, float percentage);
+int main(){
+	char name[100]={"James"};
+	int rollno, p, c, ca, i, totalmarks, percentage;
+	printf("Input the Roll no Student : ");
+	scanf("%d", &rollno);
+	printf("Input the marks of physics : ");
+	scanf("%d", &p);
+	printf("Input the marks of Chemistry :");
+	scanf("%d", &c);
+	printf("Input the marks of Computer Application : ");
+	scanf("%d", &ca);
+	totalmarks= p+c+ca;
+	percentage=totalmarks/3;
+	print(p, c, ca, name, rollno, totalmarks, percentage);
+	return 0;
+}
+void print(int p, int c, int ca, char name[], int rollno, int totalmarks, float percentage){
+	int i;
+	printf("Roll no : %d\n", rollno);
+	printf("Name of the Student :");
+	for(i=0; i<100; i++){
+		printf("%c", name[i]);
+	}
+		printf("Marks in Physics : %d\n", p);
+		printf("Marks in Chemistry : %d\n", c);
+		printf("Marks in Computer Application : %d\n", ca);
+		printf("Total Marks are : %d\n", totalmarks);
+		printf("Percentile is : %2.2f", percentage);
+}
