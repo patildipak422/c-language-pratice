0d99303469f8c4469f48f757a8cef10f247f2120
Vilas Patil <patildipak422@gmail.com>
2020-09-10 23:37:01 +0530

    Reverse a String

--- reverse_string.c
+++ reverse_string.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-	char string[100];
+	char string[100], size, i;
 	printf("Enter the string : ");
-	scanf("%s", &string);
-	printf("%s", string);
+	scanf("%s", string);
+	size=strlen(string);
+	for(i=size; i>=0; i--){
+		printf("%c ", string[i]);
+	}
 	return 0;
 }
